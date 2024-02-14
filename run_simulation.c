cee1749b5e84d58d38b03889397cea80b7fc4eb8
Youness Zioual <crness2@gmail.com>
2024-02-14 17:49:56 +0100

    commit avoid philosophers dying

--- run_simulation.c
+++ run_simulation.c
@@ -1,38 +1,38 @@
 #include "philo.h"
 
-static void     _routine(t_philo *philo)
+static void	_routine(t_philo *philo)
 {
-        while (1)
-        {
-                if (!pickup_forks(philo))
-                        break ;
+	while (1)
+	{
+		if (!pickup_forks(philo))
+			break ;
 		increment_meals_update_lmb(philo);
-                print_status(philo, "is eating 🍝");
-                ft_usleep(philo->obj->time_eat);
+		print_status(philo, "is eating 🍝");
+		ft_usleep(philo->obj->time_eat);
 		update_meals_count(philo);
 		release_forks(philo);
 		if (!ft_sleep_think(philo))
-                        break ;
-        }
+			break ;
+	}
 }
 
-void     *routine(void *data)
+// Delay for even philosophers number to avoid simultaneous actions
+void	*routine(void *data)
 {
-        t_philo *philo;
+	t_philo	*philo;
 
-        philo = (t_philo *)data;
-	// Delay for even philosophers number to avoid simultaneous actions
-        if (!(philo->id & 1))
-                ft_usleep(10);
-        _routine(philo);
-        return (NULL);
+	philo = (t_philo *)data;
+	if (!(philo->id & 1))
+		ft_usleep(10);
+	_routine(philo);
+	return (NULL);
 }
 
-void    run_simulation(int ac, char **av)
+void	run_simulation(int ac, char **av)
 {
-        t_obj   obj;
+	t_obj	obj;
 
-	init_simulation(ac, av, &obj);	
+	init_simulation(ac, av, &obj);
 	create_threads(&obj);
 	check_simulation_end(&obj);
 	wait_for_threads_finish(&obj);
--- run_simulation_utils.c
+++ run_simulation_utils.c
@@ -1,42 +1,43 @@
 #include "philo.h"
 
-void    init_simulation(int ac, char **av, t_obj *obj)
+void	init_simulation(int ac, char **av, t_obj *obj)
 {
-        memset(obj, 0, sizeof(t_obj));
-        init(ac, av, obj);
+	memset(obj, 0, sizeof(t_obj));
+	init(ac, av, obj);
 }
 
-void    create_threads(t_obj *obj)
+void	create_threads(t_obj *obj)
 {
-        int     i;
+	int	i;
 
-        i = -1;
-        while (++i < obj->num_philos)
-                pthread_create(&obj->philos[i].thread, NULL, routine, &obj->philos[i]);
+	i = -1;
+	while (++i < obj->num_philos)
+		pthread_create(&obj->philos[i].thread, NULL, routine, &obj->philos[i]);
 }
 
-void    wait_for_threads_finish(t_obj *obj)
+void	wait_for_threads_finish(t_obj *obj)
 {
-        int     i;
-        i = -1;
-        while (++i < obj->num_philos)
-                pthread_join(obj->philos[i].thread, NULL);
+	int	i;
+
+	i = -1;
+	while (++i < obj->num_philos)
+		pthread_join(obj->philos[i].thread, NULL);
 }
 
-void    cleanup_mutex(t_obj *obj)
+void	cleanup_mutex(t_obj *obj)
 {
-        int     i;
+	int	i;
 
-        i = -1;
-        while (++i < obj->num_philos)
-                pthread_mutex_destroy(&obj->philos[i].left_fork);
+	i = -1;
+	while (++i < obj->num_philos)
+		pthread_mutex_destroy(&obj->philos[i].left_fork);
 }
 
-void    check_simulation_end(t_obj *obj)
+void	check_simulation_end(t_obj *obj)
 {
-        int     i;
+	int	i;
 
-        i = -1;
-        while (++i < obj->num_philos && !check_simulation_state(obj, &i))
-                i = i + 0;
+	i = -1;
+	while (++i < obj->num_philos && !check_simulation_state(obj, &i))
+		i = i + 0;
 }
